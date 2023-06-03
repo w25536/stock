ebfa5185dfc3d62f4733b53a0ebe59d78265be75
Jungho Suh <w25536@gmail.com>
2023-06-03 16:45:07 +0900

    Updated code

---
+++ Main.py
@@ -0,0 +1,101 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from datetime import datetime
+
+data = pd.read_csv('./TSLA.csv')
+print(data.shape)
+print(data.sample(7))
+
+data['date'] = pd.to_datetime(data['date'])
+data.info()
+
+# Exploratory Data Analysis
+
+company = 'TESLA'
+
+plt.plot(data['date'], data['close'], c="r", label="close", marker="+")
+plt.plot(data['date'], data['open'], c="g", label="open", marker="^")
+plt.title(f'{company}')
+plt.legend()
+plt.tight_layout()
+
+plt.show()
+
+plt.plot(data['date'], data['volume'], c='purple', marker='*')
+plt.title(f"{company} Volume")
+plt.tight_layout()
+
+prediction_range = data.loc[(data['date'] > datetime(2013, 1, 1)) & (data['date'] < datetime(2018, 1, 1))]
+plt.plot(data['date'], data['close'])
+plt.xlabel("Date")
+plt.ylabel("Close")
+plt.title("TESLA Stock Prices")
+plt.show()
+
+close_data = data.filter(['close'])
+dataset = close_data.values
+training = int(np.ceil(len(dataset) * .95))
+print(training)
+
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaler.fit_transform(dataset)
+
+train_data = scaled_data[0:int(training), :]
+# prepare feature and labels
+x_train = []
+y_train = []
+
+for i in range(60, len(train_data)):
+    x_train.append(train_data[i - 60:i, 0])
+    y_train.append(train_data[i, 0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+model = keras.models.Sequential()
+model.add(keras.layers.LSTM(units=64, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(keras.layers.LSTM(units=64))
+model.add(keras.layers.Dense(32))
+model.add(keras.layers.Dropout(0.5))
+model.add(keras.layers.Dense(1))
+model.summary
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+history = model.fit(x_train, y_train, epochs=10)
+
+test_data = scaled_data[training - 60:, :]
+x_test = []
+y_test = dataset[training:, :]
+for i in range(60, len(test_data)):
+    x_test.append(test_data[i - 60:i, 0])
+
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+# predict the testing data
+predictions = model.predict(x_test)
+predictions = scaler.inverse_transform(predictions)
+
+# evaluation metrics
+mse = np.mean(((predictions - y_test) ** 2))
+print("MSE", mse)
+print("RMSE", np.sqrt(mse))
+
+train = data[:training]
+test = data[training:]
+test['Predictions'] = predictions
+
+plt.figure(figsize=(10, 8))
+plt.plot(train['date'], train['close'])
+plt.plot(test['date'], test[['close', 'Predictions']])
+plt.title(f'{company} Stock Close Price')
+plt.xlabel('date')
+plt.ylabel("close")
+plt.legend(['Train', 'Test', 'Predictions'])
+
+plt.show()
